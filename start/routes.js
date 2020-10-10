079a641f6ddf317a4a02359cdd966341e2eac2e2
Márcio Silva <mapsiva@gmail.com>
2020-10-09 21:55:39 -0400

    versão 1.0 do backend

--- start/routes.js
+++ start/routes.js
@@ -1,33 +1,47 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+// Area publica
+Route.post('/signup', 'UserController.signup');
+Route.post('/login', 'UserController.login');
 
+// Account
 Route.group(() => {
-  Route.resource('users', 'UserController').apiOnly();
+  Route.get('/me', 'UserController.me')
+  Route.put('/update_profile', 'UserController.updateProfile')
+  Route.put('/change_password', 'UserController.changePassword')
+})
+  .prefix('account')
+  .middleware(['auth'])
 
-  Route.post('/auth', 'AuthController.authenticate');
-});
+// Timeline
+Route.group(() => {
+  Route.get('/timeline', 'UserController.timeline')
+  Route.get('/users_to_follow', 'UserController.usersToFollow')
+  Route.post('/follow', 'UserController.follow')
+  Route.delete('/unfollow/:id', 'UserController.unFollow')
+})
+  .prefix('users')
+  .middleware(['auth']);
 
-
+// Tweet actions
 Route.group(() => {
   Route.post('/tweet', 'TweetController.tweet')
   Route.get('/tweets/:id', 'TweetController.show')
   Route.post('/tweets/reply/:id', 'TweetController.reply')
   Route.delete('/tweets/destroy/:id', 'TweetController.destroy')
 }).middleware(['auth'])
+
+// tweet reactions
+Route.group(() => {
+  // favorite tweet
+  Route.post('/create', 'FavoriteController.favorite')
+  // unfavorite tweet
+  Route.delete('/destroy/:id', 'FavoriteController.unFavorite')
+})
+  .prefix('favorites')
+  .middleware(['auth'])
+
+// User profile
+Route.get(':username', 'UserController.showProfile')
